41218a0c1494d3e8ae4155fd9a0b8f392bd4075d
Bayurzx <bayurzx@gmail.com>
2021-07-19 11:29:21 +0100

    register; login; and mobile-responsive

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,46 @@
 <template>
-    <div class="w-full">
-        <div class="text-center">
-            <h1 class="text-primary font-bold text-2xl">SIGN IN</h1>
-            <p class="text-sm text-grey opacity-75 mt-3">Welcome back! Good to have you here</p>
+    <div class="wd_contact_wrapper wd-fixer">
+        <!-- <div class="wd_overlay"></div> -->
+        <div class="wd_form">
+
+            <div class="wd_layer">
+                <div class="text-center">
+                    <h1 class="text-primary font-bold text-black text-2xl">SIGN IN</h1>
+                    <p class="text-sm text-black mt-3">Welcome back! Good to have you here</p>
+                </div>
+
+                <form class="w-11/12  mx-auto mt-10" @submit.prevent="submitForm">
+                    <div>
+                        <text-input placeholder="Email address" type="email" required v-model="email" />
+                    </div>
+                    <div class="mt-6">
+                        <text-input placeholder="Password" type="password" required v-model="password" />
+                    </div>
+                    <div class="text-right my-3">
+                        <router-link to="/forgot-password" class="text-black text-xs"
+                            >Forgot password?</router-link
+                        >
+                    </div>
+
+                    <div class="mt-6">
+                        <Button :loading="loading">
+                            Continue
+                        </Button>
+                    </div>
+                </form>
+
+                <div class="text-center mt-8">
+                    <p class="text-black text-sm">
+                        You don’t have an account yet?
+                        <router-link to="/register" class="underline text-red-secondary">Register now</router-link>
+                    </p>
+                </div>
+            </div>
         </div>
 
-        <form class="w-11/12 md:w-7/12 lg:w-5/12 xl:w-1/4 mx-auto mt-10" @submit.prevent="submitForm">
-            <div>
-                <text-input placeholder="Email address" type="email" required v-model="email" />
-            </div>
-            <div class="mt-6">
-                <text-input placeholder="Password" type="password" required v-model="password" />
-            </div>
-            <div class="text-right my-3">
-                <router-link to="/forgot-password" class="text-grey text-xs hover:text-primary duration-500"
-                    >Forgot password?</router-link
-                >
-            </div>
-
-            <div class="mt-6">
-                <Button :loading="loading">
-                    Continue
-                </Button>
-            </div>
-        </form>
-
-        <div class="text-center mt-8">
-            <p class="text-grey text-sm">
-                You don’t have an account yet?
-                <router-link to="/register" class="underline text-red-secondary">Register now</router-link>
-            </p>
-        </div>
+            
     </div>
+
 </template>
 
 <script>
