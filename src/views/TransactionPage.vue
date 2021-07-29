460115ae525b95adb9d0f47c46d23a4dcee994b2
Bayurzx <bayurzx@gmail.com>
2021-07-29 19:53:39 +0100

    minor fix

--- src/views/TransactionPage.vue
+++ src/views/TransactionPage.vue
@@ -5,7 +5,8 @@
                 <div class="bg-white shadow-md rounded my-6">
                     <table class="min-w-max w-full table-auto">
                         <thead class="bg-gray-600">
-                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal"
+                                style="color:#ED6D6D">
                                 <th class="py-3 px-6 text-left">Reference</th>
                                 <th class="py-3 px-6 text-left">Type</th>
                                 <th class="py-3 px-6 text-center">Amount</th>
